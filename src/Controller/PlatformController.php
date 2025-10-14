af7d0bdfc550a125a7b565c90e3a5a817edf9fb1
Harkály Gergő <git@harkalygergo.hu>
2025-10-14 17:21:51 +0200

    login template created with translations

--- src/Controller/PlatformController.php
+++ src/Controller/PlatformController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class PlatformController extends AbstractController
 {
-    #[Route('/platform', name: 'app_platform')]
-    public function index(): Response
+    #[Route('/', name: 'app_platform')]
+    public function index(TranslatorInterface $translator): Response
     {
-        return $this->render('platform/index.html.twig', [
+        return $this->render('platform/auth.html.twig', [
             'controller_name' => 'PlatformController',
+            'ID' => $translator->trans('ID'),
+            'Password' => $translator->trans('password'),
         ]);
     }
 }
